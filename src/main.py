356c02a806e1b968caf2eaac323555827f5b2dd5
Yan Avlis <codeyan10@gmail.com>
2024-08-04 09:51:12 -0300

    Testing cplex's MILP solver.

--- src/main.py
+++ src/main.py
@@ -6,14 +6,14 @@ def solve_linear_program():
     problem = cplex.Cplex()
 
     # Set the problem to be a maximization problem
-    problem.set_problem_type(cplex.Cplex.problem_type.LP)
+    problem.set_problem_type(cplex.Cplex.problem_type.MILP)
 
     # Objective function coefficients
     problem.objective.set_sense(problem.objective.sense.maximize)
-    problem.variables.add(obj=[3.0, 4.0], names=["x", "y"])
+    problem.variables.add(obj=[4, 5], names=["x", "y"])
 
     # Constraints coefficients and bounds
-    constraints = [[["x", "y"], [1.0, 2.0]], [["x", "y"], [3.0, 1.0]]]
+    constraints = [[["x", "y"], [3, 2]], [["x", "y"], [3.0, 1.0]]]
     rhs = [8.0, 9.0]
     senses = [
         "L",
