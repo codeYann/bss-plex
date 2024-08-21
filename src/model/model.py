87b26712f8dd4915f27c86040f211f978c5ac58c
Yan Rodrigues <yan.rodriguescs@outlook.com>
2024-08-21 08:27:36 -0300

    BSS model definition

---
+++ src/model/model.py
@@ -0,0 +1,90 @@
+import mip
+import numpy as np
+from typing import Set, Dict, Tuple
+
+
+class BSS:
+    """
+    Bike Sharing System (BSS) as a Vehicle Routing Problem (VRP).
+
+    Inputs:
+    - V: Set of vertices (stations)
+    - A: Set of arcs represented as a dictionary with (i, j) as keys and cost as values
+    - m: Number of vehicles
+    - Q: Vehicle capacity
+    - q: Demand at each vertex (list)
+    - c: Cost matrix representing the cost of traveling between stations
+    """
+
+    def __init__(
+        self,
+        V: Set[int],
+        A: Dict[Tuple[int, int], int],
+        m: int,
+        Q: int,
+        q: np.ndarray,
+        c: np.matrix,
+    ) -> None:
+        self.V = V
+        self.A = A
+        self.n = len(V)
+        self.m = m
+        self.Q = Q
+        self.q = q
+        self.c = c
+
+        # Validate inputs
+        assert len(q) == self.n, "Demand list size must match the number of vertices"
+
+        # Initialize MIP model
+        self.model = mip.Model(sense=mip.MINIMIZE, solver_name=mip.CBC)
+
+        # Set decisions variables
+        self.x = [
+            [
+                self.model.add_var(name="x_{}_{}".format(i, j), var_type=mip.BINARY)
+                for i in self.V
+            ]
+            for j in self.V
+        ]
+
+        self.set_objective_function()
+        self.base_constraints()
+
+    def set_objective_function(self) -> None:
+        """
+        Sets the objective function to minimize the total cost.
+        """
+        self.model.objective = mip.minimize(
+            mip.xsum(self.c[i, j] * self.x[i][j] for i in self.V for j in self.V)
+        )
+
+    def base_constraints(self) -> None:
+        # Constraint: Every node besides depot is visited once.
+
+        for i in self.V:
+            self.model += self.x[i][i] == 0
+
+            self.model += (
+                mip.xsum(self.x[i][j] for j in (self.V - {0}) if (i, j) in self.A) == 1,
+                "every node is viseted once on arc (i, j) except depot",
+            )
+
+            self.model += (
+                mip.xsum(self.x[j][i] for j in (self.V - {0}) if (i, j) in self.A) == 1,
+                "every node is viseted once on arc (j, i) except depot",
+            )
+
+        # Constraint: At most m vehicle leave the depot
+        self.model += (
+            mip.xsum(self.x[0][j] for j in self.V if (0, j) in self.A) <= self.m,
+            "at most m vehicle leave the depot",
+        )
+
+        # Constraint: All vehicles used must return to the depot
+        self.model += (
+            mip.xsum(self.x[0][j] for j in (self.V - {0}) if (0, j) in self.A)
+            - mip.xsum(self.x[j][0] for j in (self.V - {0}) if (0, j) in self.A)
+            == 0,
+            "all vehicles used must return to the depot",
+        )
