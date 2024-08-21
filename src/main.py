23326a1ea3784f32efe632b4614a111428f72332
Yan Rodrigues <yan.rodriguescs@outlook.com>
2024-08-21 08:28:09 -0300

    main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from loguru import logger
-from optimization.model import BSS
+from formulations.bss_mtz import Mtz
+from formulations.bss_flow import Flow
 from utils.data_set import extract_data_set_info, get_data_json
 from utils.matrix import fix_diagonal_weight
-
+import threading
 
 if __name__ == "__main__":
     try:
@@ -24,8 +25,19 @@ if __name__ == "__main__":
         Q = vehicle_capacity
         q = demands
         c = distance_matrix
+        # flow = Flow(V, A, m, Q, q, c)
+        mtz = Mtz(V, A, m, Q, q, c)
 
-        bss = BSS(V, A, m, Q, q, c)
-        bss.solve()
+        threads = [
+            # threading.Thread(target=flow.solve, args=["../logs/flow.log"]),
+            threading.Thread(target=mtz.solve, args=["../logs/mtz.log"]),
+        ]
+
+        for thread in threads:
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
     except Exception as error:
-        logger.exception(error)
+        raise error
