da17cbde949c05b5e53482da646027bc8347c824
Yan Rodrigues <yan.rodriguescs@outlook.com>
2024-08-30 13:53:25 -0300

    Implement closest neighbor heuristc

--- src/model/heuristics.py
+++ src/model/heuristics.py
@@ -1,18 +1,77 @@
-from typing import List, Dict, Tuple
+from typing import List, Dict, Tuple, Set
+import numpy as np
 
-def calculate_cumulative_demand(path: List[int], demands: Dict[int, int]) -> Tuple[int, int]:
+
+def calculate_cumulative_demand(
+    path: List[int], demands: Dict[int, int]
+) -> Tuple[int | float, int | float]:
     cumulative = 0
-    q_min, q_max = float('inf'), float('-inf')
+    q_min, q_max = float("inf"), float("-inf")
 
     for i in range(1, len(path)):
         vertex = path[i]
         cumulative += demands[vertex]
         q_min = min(q_min, cumulative)
         q_max = max(q_max, cumulative)
+
     return q_min, q_max
+
 
 def is_path_feasible(path: List[int], demands: Dict[int, int], capacity: int) -> bool:
     if len(path) == 0:
-        raise ValueError("A path must have at least 2 vertices")
+        raise ValueError("A path must have, at least, 2 vertices")
     q_min, q_max = calculate_cumulative_demand(path, demands)
     return q_max - q_min <= capacity
+
+
+def find_closest_unserved_customer(
+    current: int, customers: List[int], visited: Set[int], distances: np.matrix
+) -> int:
+    unvisited_customers = [
+        customer for customer in customers if customer not in visited
+    ]
+    if not unvisited_customers:
+        return -1
+
+    closest = min(unvisited_customers, key=lambda c: distances[current, c])
+
+    return closest
+
+
+def closest_neighbor(
+    depot: int,
+    customers: List[int],
+    demands: Dict[int, int],
+    distances: np.matrix,
+    capacity: int,
+) -> List[List[int]]:
+    routes: List[List[int]] = []
+    visited: Set[int] = set()
+
+    while customers:
+        route = [depot]
+        current = depot
+        while True:
+            next_customer = find_closest_unserved_customer(
+                current, customers, visited, distances
+            )
+
+            if next_customer == -1:
+                break
+
+            extended_route = route.copy()
+            extended_route.append(next_customer)
+
+            if not is_path_feasible(extended_route, demands, capacity):
+                print(extended_route)
+                del extended_route
+                break
+
+            route.append(next_customer)
+            visited.add(next_customer)
+            customers.remove(next_customer)
+            current = next_customer
+
+        route.append(depot)
+        routes.append(route)
+    return routes
