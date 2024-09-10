0f4baad5dce1e2fa31df8964d1bd6613cbed67c5
Yan Rodrigues <yan.rodriguescs@outlook.com>
2024-09-10 09:48:59 -0300

    tests folder with heuristics

---
+++ tests/heuristics/closest_neighbor_test.py
@@ -0,0 +1,34 @@
+import pytest
+from src.heuristics.closest_neighbor import (
+    calculate_cumulative_demand,
+    is_path_feasible,
+)
+
+
+# Test calculate_cumulative_demand
+def test_calculate_cumulative_demand():
+    path = [0, 4, 3, 5, 2]
+    demands = {0: 0, 3: -5, 5: 4, 2: 2, 4: -3}
+    (q_min, q_max) = calculate_cumulative_demand(path, demands)
+    assert q_min == -8
+    assert q_max == -2
+
+
+# Test is_path_feasible
+def test_is_path_fesible():
+    capacity = 10
+    path = [0, 4, 3, 5, 2]
+    demands = {0: 0, 3: -5, 5: 4, 2: 2, 4: -3}
+    (q_min, q_max) = calculate_cumulative_demand(path, demands)
+    (q_min, q_max) = calculate_cumulative_demand(path, demands)
+
+    is_feasible = is_path_feasible(path, demands, capacity)
+
+    assert q_max - q_min == 6
+    assert is_feasible == True
+
+
+# Test is_path_feasbile with empty path
+def test_is_path_feasible_empty_path():
+    with pytest.raises(ValueError):
+        is_path_feasible([], {}, 10)
